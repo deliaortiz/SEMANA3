220b0bccb25b481604c539f6ad61d4bfdfd91400
Delia Ortiz <deliaortizservin@gmail.com>
2024-09-27 01:10:23 -0400

    Cargando mi trabajo de JavaScript

---
+++ ejercicios.js
@@ -0,0 +1,129 @@
+// Operadores de comparación
+// 1. Comprueba si 10 es mayor o igual que 9 e imprime el resultado en consola.
+function compareTenAndNine(a,b) {
+    return a >= b;
+    
+}
+let comparacion = compareTenAndNine(10,9);
+    console.log(comparacion);
+
+// 2. Comprueba si 0 es igual a 0 e imprime el resultado en consola.
+function compareZeroAndZero() {
+
+   console.log(0===0);
+
+}
+
+// 3. Comprueba si 7 es mayor que 8 y menor que 10, e imprime el resultado en consola.
+function compareSeven() {
+    console.log(7>8<10);
+}
+
+// Operadores lógicos
+// 4. Verifica si puedes comprar un producto de 1500 con un descuento del 25% teniendo 1150€ e imprime si puedes o no.
+function canBuyProduct() {
+    
+    resultado=1500 * (1 - 0.25);
+    let dineroDispionible=1150;
+    
+    console.log(resultado <= 1150? "Puedo comprar el producto" : "No me alcanza");
+}
+
+// Variables
+// 5. Crea una variable llamada `mensaje` y asígnale el valor "Hola JavaScript", luego imprime la variable en consola.
+function createMessageVariable() {
+    let mensaje= "Hola JavaScript";
+    console.log(mensaje);
+}
+
+// 6. Crea una variable llamada `resultado` y asígnale la suma de 2 y 3, luego imprime la variable en consola.
+function createSumVariable(a,b) {
+     let resultado=2+3;
+    console.log(resultado);
+}
+
+// 7. Crea una constante llamada `IS_DISABLED` y asígnale el valor booleano `true`, luego imprime la constante en consola.
+function createDisabledConstant() {
+    const IS_DISABLED=true;
+    console.log(IS_DISABLED)
+}
+
+// Null y Undefined
+// 8. Crea una variable con `let` llamada `capacidad` y asígnale un valor `null`, luego imprime la variable en consola.
+function createNullVariable() {
+    const capacidad= null;
+    console.log(capacidad);
+}
+
+// 9. Crea una variable con `let` llamada `dinero` y asígnale un valor `undefined`, luego imprime la variable en consola.
+function createUndefinedVariable() {
+
+    let dinero =undefined
+    console.log(dinero)
+}
+
+// typeof
+// 10. Escribe un código para ver el tipo de una variable llamada `userName` e imprime el tipo de dato en consola.
+function checkUserNameType() {
+    let userName;
+    console.log(userName);
+}
+
+// 11. Asegúrate de que `dogId` es una cadena de texto y luego imprime el tipo de dato en consola.
+function checkDogIdIsString(dogId) {
+    typeof dogId;String;
+    console.log(dogId);
+    
+}
+
+// console.log()
+// 12. Imprime un mensaje en la consola utilizando `console.log()`.
+function logMessage() {
+
+    console.log("Hola me llamo Delia");
+}
+
+// 13. Utiliza `console.log()` para imprimir el valor de la variable `edad` que tiene el valor 30.
+function logAge() {
+    let edad=30;
+    console.log(edad)
+}
+
+// 14. Muestra el mensaje 'La versión de JavaScript es' seguido de la variable `version` con el valor 2024 usando `console.log()`.
+function logVersion() {
+    let version=2024;
+    console.log("La versión de JavaScript es "+2024);
+
+}
+
+// Exportar todas las funciones para poder usarlas en las pruebas
+compareTenAndNine();
+compareZeroAndZero();
+compareSeven();
+canBuyProduct();
+createMessageVariable();
+createSumVariable();
+createDisabledConstant();
+createNullVariable();
+createUndefinedVariable();
+checkUserNameType();
+checkDogIdIsString();
+logMessage();
+logAge();
+logVersion();
+module.exports = {
+    compareTenAndNine,
+    compareZeroAndZero,
+    compareSeven,
+    canBuyProduct,
+    createMessageVariable,
+    createSumVariable,
+    createDisabledConstant,
+    createNullVariable,
+    createUndefinedVariable,
+    checkUserNameType,
+    checkDogIdIsString,
+    logMessage,
+    logAge,
+    logVersion,
+};
